864779b7e1e45735c09b5bc0351a2b6560890fd0
Jim <limchorngjim@gmail.com>
2023-12-13 09:37:15 +0100

    setup fastapi cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,23 @@ from app.repositories.ticket_repository import TicketRepository
 import uvicorn
 from fastapi import Depends, FastAPI
 from fastapi.responses import JSONResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-TICKET_FILEPATH = "../data/awesome_tickets.json"
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+TICKET_FILEPATH = "./data/awesome_tickets.json"
 ticket_repository = TicketRepository(filepath=TICKET_FILEPATH)
 
 
