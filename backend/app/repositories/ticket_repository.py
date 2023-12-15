418ed86008ee9ded9e9455b7ad7b7e48ea36eb1b
Jim <limchorngjim@gmail.com>
2023-12-15 15:44:48 +0100

    implement messages route methods

--- backend/app/repositories/ticket_repository.py
+++ backend/app/repositories/ticket_repository.py
@@ -1,6 +1,6 @@
 import json
 from typing import Optional
-
+from typing import List
 
 class TicketRepository:
     def __init__(self, filepath: str):
@@ -19,6 +19,25 @@ class TicketRepository:
                 break
         return ticket
 
+    def get_message(self, msgId: str) -> dict:
+        message = {}
+        messages = self.data["messages"]
+        for message in messages:
+            if message["id"] == msgId:
+                message = message
+                break
+        return message
 
-    def get_messages(self) -> list[dict]:
-        return self.data["messages"]
+    def get_conversation(self, ticketId: str) -> List[dict]:
+        conversation = []
+        context_messages = []
+        tickets = self.get_tickets()
+        messages = self.data["messages"]
+        for ticket in tickets:
+            if ticket['id'] == ticketId:
+                context_messages = ticket["context_messages"]
+
+        for message in messages:
+            if message["id"] in context_messages:
+                conversation.append(message)
+        return conversation
