b079c2ef76792361cecb4c41936b31512e4b0e74
Jim <limchorngjim@gmail.com>
2023-12-16 19:33:15 +0100

    display each ticket context messages

--- frontend/components/Ticket/index.tsx
+++ frontend/components/Ticket/index.tsx
@@ -3,6 +3,8 @@ import styles from "../../styles/Tickets.module.css";
 import { MessageType, TicketType } from "../../types/types";
 import { useEffect, useState } from "react";
 import fetcher from "../../lib/fetcher";
+import Drawer from "@mui/material/Drawer";
+import TurnRightIcon from "@mui/icons-material/TurnRight";
 
 type Props = {
 	ticket: TicketType;
@@ -12,6 +14,10 @@ type Props = {
 
 const Ticket: React.FC<Props> = (props) => {
 	const [message, setMessage] = useState<MessageType | null>(null);
+	const [toggleDrawer, setToggleDrawer] = useState(false);
+	const [contextMessages, setContextMessages] = useState<MessageType[] | null>(
+		null
+	);
 
 	useEffect(() => {
 		const fetchMessage = async () => {
@@ -22,8 +28,45 @@ const Ticket: React.FC<Props> = (props) => {
 		fetchMessage();
 	}, []);
 
+	async function handleDrawerOpen(e: React.KeyboardEvent | React.MouseEvent) {
+		if (!toggleDrawer) {
+			setToggleDrawer(true);
+
+			const data = await fetcher(`messages?ticketId=${props.ticket.id}`);
+			setContextMessages(data);
+		} else {
+			setToggleDrawer(false);
+		}
+	}
+
+	function getRepliedMessage(referenceId: string) {
+		const repliedMessage = contextMessages.find(
+			(message) => message.id === referenceId
+		);
+		if (repliedMessage) {
+			return (
+				<Box sx={{ display: "flex", alignItems: "center" }}>
+					<TurnRightIcon sx={{ m: "0 25px 0 17px", color: "#808389" }} />
+					<Box sx={{ display: "flex", alignItems: "center" }}>
+						{/* eslint-disable-next-line @next/next/no-img-element */}
+						<img
+							src={repliedMessage.author.avatar_url}
+							alt=""
+							width={20}
+							height={20}
+							style={{ borderRadius: "50%", marginRight: "6px" }}
+						/>
+						<Typography noWrap color="#dfdfe2" sx={{ fontSize: "12px" }}>
+							{repliedMessage.content}
+						</Typography>
+					</Box>
+				</Box>
+			);
+		}
+	}
+
 	return (
-		<Box className={styles.ticket}>
+		<Box className={styles.ticket} onClick={handleDrawerOpen}>
 			{message && (
 				<Box sx={{ display: "flex" }}>
 					<Typography color={message.author.color}>
@@ -41,6 +84,46 @@ const Ticket: React.FC<Props> = (props) => {
 					</button>
 				</Box>
 			)}
+			<Drawer
+				anchor="right"
+				open={toggleDrawer}
+				onClose={() => setToggleDrawer(false)}
+			>
+				<Box sx={{ width: "550px", p: "20px" }}>
+					{contextMessages &&
+						contextMessages.map((message: MessageType) => (
+							<>
+								{message.reference_msg_id &&
+									getRepliedMessage(message.reference_msg_id)}
+								<Box key={message.id} sx={{ display: "flex", mb: "20px" }}>
+									{/* eslint-disable-next-line @next/next/no-img-element */}
+									<img
+										src={message.author.avatar_url}
+										alt=""
+										width={50}
+										height={50}
+										style={{ borderRadius: "50%", marginRight: "16px" }}
+									/>
+									<Box sx={{ display: "flex", flexDirection: "column" }}>
+										<Box sx={{ display: "flex", alignItems: "center" }}>
+											<Typography
+												color={message.author.color}
+												sx={{ mr: "8px" }}
+											>
+												{message.author.name}
+											</Typography>
+											<Typography color="#808389" sx={{ fontSize: "12px" }}>
+												{message.timestamp}
+											</Typography>
+										</Box>
+										{message.content}
+									</Box>
+								</Box>
+							</>
+						))}
+					{/* <a href={message.msg_url}>Go to discord</a> */}
+				</Box>
+			</Drawer>
 		</Box>
 	);
 };
