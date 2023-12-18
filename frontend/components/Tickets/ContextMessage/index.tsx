f991d0a0654d3851ee133125af7ffdd5991fd8c9
Jim <limchorngjim@gmail.com>
2023-12-18 20:38:05 +0100

    extract context messages to separate component

---
+++ frontend/components/Tickets/ContextMessage/index.tsx
@@ -0,0 +1,110 @@
+import { Box, Typography } from "@mui/material";
+import { MessageType, TicketType } from "../../../types/types";
+import TurnRightIcon from "@mui/icons-material/TurnRight";
+import LinkIcon from "@mui/icons-material/Link";
+import ticketsPageStyles from "../../../styles/Tickets.module.css";
+import contextMessageStyles from "./ContextMessage.module.css";
+
+type Props = {
+	ticket: TicketType;
+	message: MessageType;
+	contextMessages: MessageType[];
+};
+
+const ContextMessage: React.FC<Props> = (props) => {
+	function getRepliedMessage(referenceId: string) {
+		const repliedMessage = props.contextMessages.find(
+			(message) => message.id === referenceId
+		);
+		if (repliedMessage) {
+			return (
+				<Box sx={{ display: "flex", alignItems: "center" }}>
+					<TurnRightIcon sx={{ m: "0 25px 0 37px", color: "#808389" }} />
+					<Box
+						sx={{
+							display: "flex",
+							alignItems: "center",
+							overflow: "hidden",
+							textOverflow: "ellipsis",
+							whiteSpace: "nowrap",
+						}}
+					>
+						{/* eslint-disable-next-line @next/next/no-img-element */}
+						<img
+							src={repliedMessage.author.avatar_url}
+							alt=""
+							width={20}
+							height={20}
+							style={{ borderRadius: "50%", marginRight: "6px" }}
+						/>
+						<Typography color={props.message.author.color}>
+							{repliedMessage.author.name}
+						</Typography>
+						<Typography noWrap color="#dfdfe2" sx={{ fontSize: "12px" }}>
+							: {repliedMessage.content}
+						</Typography>
+					</Box>
+				</Box>
+			);
+		}
+	}
+
+	return (
+		<Box
+			sx={{
+				bgcolor:
+					props.ticket.msg_id === props.message.id
+						? "rgba(219, 168, 50, 0.2)"
+						: "",
+			}}
+		>
+			{props.message.reference_msg_id &&
+				getRepliedMessage(props.message.reference_msg_id)}
+			<Box
+				sx={{
+					display: "flex",
+					mb: "20px",
+					p: "0 20px",
+				}}
+			>
+				{/* eslint-disable-next-line @next/next/no-img-element */}
+				<img
+					src={props.message.author.avatar_url}
+					alt=""
+					width={50}
+					height={50}
+					style={{ borderRadius: "50%", marginRight: "16px" }}
+				/>
+				<Box width="100%" sx={{ display: "flex", flexDirection: "column" }}>
+					<Box
+						sx={{
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "space-between",
+						}}
+					>
+						<Box sx={{ display: "flex" }}>
+							<Typography color={props.message.author.color} sx={{ mr: "8px" }}>
+								{props.message.author.name}
+							</Typography>
+							<Typography color="#808389" sx={{ fontSize: "12px" }}>
+								{props.message.timestamp}
+							</Typography>
+						</Box>
+						{props.ticket.msg_id === props.message.id && (
+							<a href={props.message.msg_url} style={{ border: "none" }}>
+								<LinkIcon
+									fontSize="small"
+									className={ticketsPageStyles.actionIcon}
+								/>
+							</a>
+						)}
+					</Box>
+					<Typography>{props.message.content}</Typography>
+				</Box>
+			</Box>
+		</Box>
+	);
+};
+
+export default ContextMessage;
--- frontend/components/Tickets/index.tsx
+++ frontend/components/Tickets/index.tsx
@@ -4,9 +4,8 @@ import { MessageType, TicketType } from "../../types/types";
 import { useEffect, useState } from "react";
 import fetcher from "../../lib/fetcher";
 import Drawer from "@mui/material/Drawer";
-import TurnRightIcon from "@mui/icons-material/TurnRight";
-import LinkIcon from "@mui/icons-material/Link";
 import TicketFooter from "./TicketFooter";
+import ContextMessage from "./ContextMessage";
 
 type Props = {
 	ticket: TicketType;
@@ -42,43 +41,6 @@ const Ticket: React.FC<Props> = (props) => {
 		}
 	}
 
-	function getRepliedMessage(referenceId: string) {
-		const repliedMessage = contextMessages.find(
-			(message) => message.id === referenceId
-		);
-		if (repliedMessage) {
-			return (
-				<Box sx={{ display: "flex", alignItems: "center" }}>
-					<TurnRightIcon sx={{ m: "0 25px 0 37px", color: "#808389" }} />
-					<Box
-						sx={{
-							display: "flex",
-							alignItems: "center",
-							overflow: "hidden",
-							textOverflow: "ellipsis",
-							whiteSpace: "nowrap",
-						}}
-					>
-						{/* eslint-disable-next-line @next/next/no-img-element */}
-						<img
-							src={repliedMessage.author.avatar_url}
-							alt=""
-							width={20}
-							height={20}
-							style={{ borderRadius: "50%", marginRight: "6px" }}
-						/>
-						<Typography color={message.author.color}>
-							{repliedMessage.author.name}
-						</Typography>
-						<Typography noWrap color="#dfdfe2" sx={{ fontSize: "12px" }}>
-							: {repliedMessage.content}
-						</Typography>
-					</Box>
-				</Box>
-			);
-		}
-	}
-
 	return (
 		<Grid item md={6} xs={12}>
 			<Box
@@ -110,70 +72,15 @@ const Ticket: React.FC<Props> = (props) => {
 					onClose={() => setToggleDrawer(false)}
 					hideBackdrop
 				>
-					<Box sx={{ width: "500px", p: "20px 0" }}>
+					<Box className={styles.drawerContainer}>
 						{contextMessages &&
 							contextMessages.map((message: MessageType) => (
-								<Box
+								<ContextMessage
 									key={message.id}
-									sx={{
-										bgcolor:
-											props.ticket.msg_id === message.id
-												? "rgba(219, 168, 50, 0.2)"
-												: "",
-									}}
-								>
-									{message.reference_msg_id &&
-										getRepliedMessage(message.reference_msg_id)}
-									<Box
-										sx={{
-											display: "flex",
-											mb: "20px",
-											p: "0 20px",
-										}}
-									>
-										{/* eslint-disable-next-line @next/next/no-img-element */}
-										<img
-											src={message.author.avatar_url}
-											alt=""
-											width={50}
-											height={50}
-											style={{ borderRadius: "50%", marginRight: "16px" }}
-										/>
-										<Box
-											width="100%"
-											sx={{ display: "flex", flexDirection: "column" }}
-										>
-											<Box
-												sx={{
-													display: "flex",
-													alignItems: "center",
-													justifyContent: "space-between",
-												}}
-											>
-												<Box sx={{ display: "flex" }}>
-													<Typography
-														color={message.author.color}
-														sx={{ mr: "8px" }}
-													>
-														{message.author.name}
-													</Typography>
-													<Typography color="#808389" sx={{ fontSize: "12px" }}>
-														{message.timestamp}
-													</Typography>
-												</Box>
-												{props.ticket.msg_id === message.id && (
-													<a href={message.msg_url} style={{ border: "none" }}>
-														<LinkIcon
-															fontSize="small"
-															className={styles.actionIcon}
-														/>
-													</a>
-												)}
-											</Box>
-											{message.content}
-										</Box>
-									</Box>
-								</Box>
+									ticket={props.ticket}
+									message={message}
+									contextMessages={contextMessages}
+								/>
 							))}
 					</Box>
 				</Drawer>
